d6c3af488230a6c924dc7b9f341694d0780557ca
Ricardo Soares Nogueira <ricardosn87@hotmail.com>
2020-02-04 08:19:32 -0300

    DevTestGlobo

--- MetaGloboDevTest/MetaGloboDevTest/Startup.cs
+++ MetaGloboDevTest/MetaGloboDevTest/Startup.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 using AutoMapper;
 using MetaGloboDevTest.Context;
 using MetaGloboDevTest.DTOs.Mappings;
@@ -15,15 +11,11 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
@@ -42,7 +34,8 @@ namespace MetaGloboDevTest
         public void ConfigureServices(IServiceCollection services)
         {
 
-            var mappingConfig = new MapperConfiguration(mappingConfig => {
+            var mappingConfig = new MapperConfiguration(mappingConfig =>
+            {
                 mappingConfig.AddProfile(new MappingProfile());
             });
 
@@ -77,7 +70,7 @@ namespace MetaGloboDevTest
                 {
                     Version = " 1.0.0",
                     Title = "Contato",
-                    Description = " API para um serviço de gestão de contatos",                   
+                    Description = " API para um serviço de gestão de contatos",
                     Contact = new OpenApiContact
                     {
                         Name = "Developer Team",
@@ -91,9 +84,35 @@ namespace MetaGloboDevTest
                     }
                 });
 
-                //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-                //c.IncludeXmlComments(xmlPath);
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description =
+         "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+
             });
 
             services.AddControllers().AddNewtonsoftJson(options =>
