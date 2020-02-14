c408db6cd87903b493727be1f453b1a439b692d5
Ricardo Soares Nogueira <ricardosn87@hotmail.com>
2020-02-14 00:43:09 -0300

    Demo_DBFirst

---
+++ .Net Core/EntityFramework/Demo_DBFirst/Demo_DBFirst/Models/Produtos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo_DBFirst.Models
+{
+    public partial class Produtos
+    {
+        public int ProdutoId { get; set; }
+        public string ProdutoNome { get; set; }
+        public decimal ProdutoPreco { get; set; }
+        public string ProdutoCategoria { get; set; }
+    }
+}
