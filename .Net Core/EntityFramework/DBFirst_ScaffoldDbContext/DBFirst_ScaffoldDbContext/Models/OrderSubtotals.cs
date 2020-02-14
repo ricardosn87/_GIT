130b07c6b5951e18aa9d681a34a968afe4341595
Ricardo Soares Nogueira <ricardosn87@hotmail.com>
2020-02-14 01:04:01 -0300

    DBFirst_ScaffoldDbContext

---
+++ .Net Core/EntityFramework/DBFirst_ScaffoldDbContext/DBFirst_ScaffoldDbContext/Models/OrderSubtotals.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirst_ScaffoldDbContext.Models
+{
+    public partial class OrderSubtotals
+    {
+        public int OrderId { get; set; }
+        public decimal? Subtotal { get; set; }
+    }
+}
---
+++ .Net Core/EntityFramework/DBFirst_ScaffoldDbContext/DBFirst_ScaffoldDbContext/Models/SummaryOfSalesByYear.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirst_ScaffoldDbContext.Models
+{
+    public partial class SummaryOfSalesByYear
+    {
+        public DateTime? ShippedDate { get; set; }
+        public int OrderId { get; set; }
+        public decimal? Subtotal { get; set; }
+    }
+}
