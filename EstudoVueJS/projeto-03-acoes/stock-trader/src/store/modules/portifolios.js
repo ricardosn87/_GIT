4226b007a35fc7c99f8e203d0bebf57ddbdadded
Ricardo Soares Nogueira <ricardo.nogueira@grupomult.com.br>
2022-06-08 22:32:52 -0300

    08-06-2022

--- EstudoVueJS/projeto-03-acoes/stock-trader/src/store/modules/portifolios.js
+++ EstudoVueJS/projeto-03-acoes/stock-trader/src/store/modules/portifolios.js
@@ -37,7 +37,7 @@ export default {
     getters: {
         stockPortifolio(state, getters) {
             return state.stocks.map(stock => {
-                 const record = getters.stocks.find(element => element.id = stock.id)                               
+                 const record = getters.stocks.find(element => element.id == stock.id)                               
                 return {
                     id: stock.id,
                     quantity: stock.quantity,
