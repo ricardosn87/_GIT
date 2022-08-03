eec2d55b2a3424ae118a8efeca892a249733ede3
Ricardo Soares Nogueira <ricardo.nogueira@grupomult.com.br>
2022-08-03 15:28:57 -0300

    2022-08-03

--- EstudoVueJS/baseconhecimento/backend/knexfile.js
+++ EstudoVueJS/baseconhecimento/backend/knexfile.js
@@ -5,7 +5,13 @@ module.exports = {
 	connection: db,
 	pool: {
 		min: 2,
-		max: 10
+		max: 10,
+		idleTimeoutMillis: 30000,
+		createTimeoutMillis: 3000,
+		acquireTimeoutMillis: 30000,
+		reapIntervalMillis: 1000,
+		createRetryIntervalMillis: 100,
+		propagateCreateError: false
 	},
 	migrations: {
 		tableName: 'knex_migrations'
