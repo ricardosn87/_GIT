e7107d6b3f25a9120ebf6c3029c68726c2e9c584
Ricardo Soares Nogueira <ricardo.nogueira@grupomult.com.br>
2022-12-26 20:56:36 -0300

    26-12-2022

--- FormacaoNode/GuiaPerguntas/index.js
+++ FormacaoNode/GuiaPerguntas/index.js
@@ -1,21 +1,19 @@
 const express = require("express")
 const app = express();
 
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
+app.use(express.static('public'))
 
-app.get("/:nome/:lang",(req,res) =>{
+app.get("/", (req, res) => {
 
-    var nome = req.params.nome
-    var lang = req.params.lang
-
-    res.render("index",{
-         nome:nome,
-         lang:lang,
-         empresa: "Guia do programador",
-         inscritos: 8000
-    })
+    res.render("index")
 })
 
-app.listen(8080,()=>{
+app.get("/perguntar", (req, res) => {
+
+    res.render("perguntar")
+})
+
+app.listen(8080, () => {
     console.log("App Rodando")
 })
